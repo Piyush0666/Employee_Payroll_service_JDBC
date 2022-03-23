b2fe364b56580681ceb96534a90f8f2f434934cc
your_github_username <your_github_email>
2022-03-23 17:19:26 +0530

    EMPLOYEE_PAYROLL_SERVICE_CRUD_OPERATION_USING_JDBC

--- src/main/java/com/bridgelabz/Day34/main.java
+++ src/main/java/com/bridgelabz/Day34/main.java
@@ -4,6 +4,6 @@ public class main
 {
     public static void main(String[] args)
     {
-        System.out.println("WELCOME TO EMPLOYEE PAYROLL SERVICE");
+        System.out.println("WELCOME TO EMPLOYEE PAYROLL SERVICE ");
     }
 }
---
+++ src/test/java/com/bridgelabz/Day34/AppTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Day34;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+class AppTest extends TestCase
+{
+        public AppTest( String testName )
+        {
+            super( testName );
+        }
+
+        public static Test suite()
+        {
+            return (Test) new TestSuite( AppTest.class );
+        }
+
+        public void testApp()
+        {
+            assertTrue( true );
+        }
+
+}
